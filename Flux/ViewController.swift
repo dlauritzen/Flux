7110866e0ea5de8cbffa1701cccdb298a6b67f1f
Dallin Lauritzen <dallin@dallinlauritzen.com>
2015-03-04 16:51:24 -0700

    Initial Commit

---
+++ Flux/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Flux
+//
+//  Created by Dallin Lauritzen on 3/4/15.
+//  Copyright (c) 2015 Dallin Lauritzen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
